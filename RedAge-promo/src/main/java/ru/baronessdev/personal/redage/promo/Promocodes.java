1de289ee2eb45bf77cd0a8ebec51bd44e5a2e1da
BlackBaroness <shoyurash2@gmail.com>
2021-06-24 21:40:35 +0300

    commit

---
+++ RedAge-promo/src/main/java/ru/baronessdev/personal/redage/promo/Promocodes.java
@@ -0,0 +1,11 @@
+package ru.baronessdev.personal.redage.promo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Promocodes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
