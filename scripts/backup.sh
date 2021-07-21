7e333e40e66e43fe3aeb66c94f85a8974a448c55
Black_Baroness <57033600+BlackBaroness@users.noreply.github.com>
2021-07-21 22:06:46 +0300

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -13,7 +13,7 @@ apt install -y p7zip-full lftp &&
 # сборка всех серверов в архив
 mysqldump -u root -pPassword --all-databases > databases.sql &&
 TIME=$(date +%F-%T)
-7z a "$TIME".7z proxy lobby server-1 databases.sql 2> backupLogs/"$TIME".log &&
+7z a "$TIME".7z proxy lobby server-1 databases.sql &&
 
 # отправка
 lftp ftp://$USER:$PASSWORD@backup.s1.fsn.spacecore.pro:21 -e "set ftp:ssl-allow no; put -O / $TIME.7z; quit" &&
